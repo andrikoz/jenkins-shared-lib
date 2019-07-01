e162ad58d0e679b2050d32e7038f8290cd17fb49
agounaris <andreas.gounaris@theblueground.com>
2019-07-01 10:20:56 +0300

    Refactor updateGithubCommitStatus to resolve multi scm projects

---
+++ vars/statusCheck.groovy
@@ -0,0 +1,36 @@
+/**
+ * Send status to Github via the Status API
+ *
+ * @param build - currentBuild, jenkins env variable
+ */
+
+def getRepoURL() {
+  sh "git config --get remote.origin.url > .git/remote-url"
+  return readFile(".git/remote-url").trim()
+}
+
+def getCommitSha() {
+  sh "git rev-parse HEAD > .git/current-commit"
+  return readFile(".git/current-commit").trim()
+}
+
+def updateGithubCommitStatus(build) {
+  // workaround https://issues.jenkins-ci.org/browse/JENKINS-38674
+  repoUrl = getRepoURL()
+  commitSha = getCommitSha()
+
+  step([
+      $class: 'GitHubCommitStatusSetter',
+      reposSource: [$class: "ManuallyEnteredRepositorySource", url: repoUrl],
+      commitShaSource: [$class: "ManuallyEnteredShaSource", sha: commitSha],
+      errorHandlers: [[$class: 'ShallowAnyErrorHandler']],
+      statusResultSource: [
+          $class: 'ConditionalStatusResultSource',
+          results: [
+              [$class: 'BetterThanOrEqualBuildResult', result: 'SUCCESS', state: 'SUCCESS', message: "Build Succeeded"],
+              [$class: 'BetterThanOrEqualBuildResult', result: 'FAILURE', state: 'FAILURE', message: "Build failed"],
+              [$class: 'AnyBuildResult', state: 'FAILURE', message: 'Loophole']
+          ]
+      ]
+  ])
+}
